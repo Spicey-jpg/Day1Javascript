e3089f5f3ff03a8ab1f7a436bd9f21e794aa0795
Spicey-jpg <williamoctober210@gmail.com>
2024-11-11 11:48:19 +0200

    added new js

---
+++ variables.js
@@ -0,0 +1,70 @@
+// Variables
+// let name = "Willmeister"]
+// const name = "Willmeister"]
+// var name = "Willmeister"]
+//------------------------------------
+// number
+// let number1 = 85.3
+//let value = true //false
+//------------------------------------
+//undefined
+//let name
+//------------------------------------
+//null
+//let value = null
+//suff
+//let name = true
+//let num = 5
+//console.log(`My name is ${name}`);
+//console.log("My name is " + name +num);
+//console.log(name);
+
+//Array
+// let myArray = ['anything',2, true, 'string']
+// console.log(myArray[0]); //indexing
+
+//camelcase -> myArray(always used in JS)
+//pascalcase -> MyArray
+//kebabcase -> my-array(only use in vue)
+
+// object literal -declare(make) an object
+// let table = {
+    // material:"wood",
+    // no_of_legs:4,
+    // occupied: true
+// }
+//bracket notation
+// console.log(table['material']);
+//dot notation
+// console.log(table.material);
+//destructuring
+// let {material} = table//wood
+// console.log(material);
+
+//funtion
+// function greeting(a,b){
+    // console.log('This is a greeting'+ (a+b));
+    
+// }
+
+// greeting(3,4)
+
+// let num1 = 5
+//++num1 pre increment
+//num1++ post increment
+//--num1 pre decrement
+//num1-- post decrement
+//console.log(num1++);
+// console.log(6!=='6');
+
+let myString = "Something"
+// shows length of string
+let length = myString.length
+// make text capital --> myString.toUpperCase()
+// make text small letter --> myString.toLowerCase()
+let split = myString.toLowerCase().split('e')
+console.log(split);
+
+
+
+
